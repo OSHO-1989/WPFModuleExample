2574a54e7f5b7ecf789e77f22684bb52d9842d6e
肖小龙 <xiaoxiaolong@hikvision.com.cn>
2022-12-19 16:18:54 +0800

    提交自定义标题栏示例代码（自定义应用程序图标、应用程序名称、最小化、最大化和关闭按钮）

---
+++ CustomizeWindowTitleBar/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomizeWindowTitleBar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //记录鼠标点击的位置
+        private Point _pressedPos;
+
+        //记录鼠标是否正在拖动
+        bool _isDragMoved = false;
+
+        //自定义标题栏的高度
+        readonly double _customizeTitleBarHeight = 60;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _pressedPos = e.GetPosition(this);
+        }
+
+        private void Window_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_isDragMoved)
+            {
+                _isDragMoved = false;
+                e.Handled = true;
+            }
+        }
+
+        private void Window_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed && _pressedPos != e.GetPosition(this))
+            {
+                if (_pressedPos.Y > 0 && _pressedPos.Y < _customizeTitleBarHeight)
+                {
+                    _isDragMoved = true;
+                    DragMove();
+                }
+            }
+        }
+
+        private void MinButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaxButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = (this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized);
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowTitleGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount > 1)
+            {
+                this.WindowState = (this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized);
+            }
+        }
+    }
+}
